ff7ca14ed3d14aac76e609038f2c8f54a507e775
eunseo <qnrqjrm03@naver.com>
2023-05-14 04:37:52 +0900

    feat: 초기 구조 세팅, 로그인

---
+++ EnjoyTrip-Front/src/components/TheHeaderNavbar.vue
@@ -0,0 +1,210 @@
+<template>
+  <div>
+    <nav
+      class="navbar navbar-expand-lg navbar-dark"
+      style="background-color: #eaeaea; padding: 0% 10% 0% 10%"
+    >
+      <router-link :to="{ name: 'main' }" class="navbar-brand">
+        <img
+          :src="require('@/assets/logo.png')"
+          id="logo"
+          class="d-inline-block align-top"
+          alt="로고"
+        />
+      </router-link>
+
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#nav-collapse"
+        aria-controls="nav-collapse"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="nav-collapse">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <router-link :to="{ name: 'main' }" class="nav-link">
+              <i
+                class="bi bi-house-door"
+                style="font-size: 2rem; animation: fade"
+              ></i>
+              홈
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link :to="{ name: 'house' }" class="nav-link">
+              <i
+                class="bi bi-house-fill"
+                style="font-size: 2rem; animation: fade"
+              ></i>
+              여행지 검색
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link :to="{ name: 'house' }" class="nav-link">
+              <i
+                class="bi bi-house-fill"
+                style="font-size: 2rem; animation: fade"
+              ></i>
+              여행 계획
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link :to="{ name: 'board' }" class="nav-link">
+              <i
+                class="bi bi-journal"
+                style="font-size: 2rem; animation: fade"
+              ></i>
+              여행지 공유
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link :to="{ name: 'insta' }" class="nav-link">
+              <i
+                class="bi bi-instagram"
+                style="font-size: 2rem; animation: fade"
+              ></i>
+              핫 플레이스
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link :to="{ name: 'board' }" class="nav-link">
+              <i
+                class="bi bi-journal"
+                style="font-size: 2rem; animation: fade"
+              ></i>
+              공지사항
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link :to="{ name: 'todo' }" class="nav-link">
+              <i
+                class="bi bi-calendar-check"
+                style="font-size: 2rem; animation: fade"
+              ></i>
+              TodoList
+            </router-link>
+          </li>
+        </ul>
+
+        <!-- after login -->
+        <ul class="navbar-nav ml-auto" v-if="userInfo">
+          <li class="nav-item align-self-center">
+            <div class="avatar primary-bg">
+              {{ userInfo.userid.charAt(0).toUpperCase() }}
+            </div>
+          </li>
+          <li class="nav-item align-self-center">
+            <span
+              class="navbar-text"
+              style="color: #333; font-family: 'Arial', sans-serif"
+            >
+              {{ userInfo.username }}({{ userInfo.userid }})님 환영합니다.
+            </span>
+          </li>
+          <li class="nav-item align-self-center">
+            <router-link
+              :to="{ name: 'mypage' }"
+              class="nav-link"
+              style="color: #333; font-family: 'Arial', sans-serif"
+            >
+              내 정보 보기
+            </router-link>
+          </li>
+          <li class="nav-item align-self-center">
+            <a href="#" class="nav-link" @click.prevent="onClickLogout"
+              >로그아웃</a
+            >
+          </li>
+        </ul>
+
+        <!-- before login -->
+        <ul class="navbar-nav ml-auto" v-else>
+          <li class="nav-item">
+            <router-link :to="{ name: 'join' }" class="nav-link">
+              <i class="bi bi-person-circle"></i> 회원가입
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link :to="{ name: 'login' }" class="nav-link">
+              <i class="bi bi-key"></i> 로그인
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from "vuex";
+
+const memberStore = "memberStore";
+
+export default {
+  name: "TheHeaderNavbar",
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapState(memberStore, ["userInfo"]),
+  },
+  methods: {
+    ...mapActions(memberStore, ["userLogout"]),
+    onClickLogout() {
+      console.log(this.userInfo.userid);
+      this.userLogout(this.userInfo.userid);
+      sessionStorage.removeItem("access-token");
+      sessionStorage.removeItem("refresh-token");
+      if (this.$route.path !== "/") this.$router.push({ name: "main" });
+    },
+  },
+};
+</script>
+
+<style scoped>
+#logo {
+  width: 240px;
+}
+
+.link {
+  text-decoration: none;
+}
+
+.primary-bg {
+  background-color: #467cc2;
+  color: #fff;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: bold;
+  font-size: 1.2rem;
+}
+
+.avatar {
+  margin-right: 10px;
+}
+
+.navbar-text {
+  margin-left: 5px;
+}
+</style>
+
+<style scoped>
+.navbar-dark .navbar-nav .nav-link {
+  color: #333;
+  font-family: "Arial", sans-serif;
+}
+
+/* .nav-link:hover {
+  color: #467cc2 !important;
+} */
+</style>
---
+++ EnjoyTrip-Front/src/views/AppMain.vue
@@ -0,0 +1,67 @@
+<template>
+  <section>
+    <!-- 관광지 드롭다운메뉴 시작-->
+    <div class="background" style="padding: 1%">
+      <div class="list-container">
+        <header class="bg-dark py-5">
+          <div class="container px-5">
+            <div class="row gx-5 align-items-center justify-content-center">
+              <div class="col-lg-8 col-xl-7 col-xxl-6">
+                <div class="my-5 text-center text-xl-start">
+                  <h1 class="display-5 fw-bolder text-white mb-2">
+                    Enjoy Trip으로
+                  </h1>
+                  <h1 class="display-5 fw-bolder text-white mb-2">
+                    여행을 시작해보세요!
+                  </h1>
+                  <p class="lead fw-normal text-white-50 mb-4">
+                    지역별 다양한 여행지를 확인해보고, 나만의 여행지를 만들어
+                    여행 계획을 세워보세요
+                  </p>
+                  <div
+                    class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xl-start"
+                  >
+                    <!-- <a class="btn btn-primary btn-lg px-4 me-sm-3" href="#features">Get Started</a> -->
+                    <a
+                      class="btn btn-outline-light btn-lg px-4"
+                      href="${root}/attraction?action=searchtrip"
+                      >Go Search</a
+                    >
+                  </div>
+                </div>
+              </div>
+              <div class="col-xl-5 col-xxl-6 d-none d-xl-block text-center">
+                <img
+                  class="img-fluid rounded-3 my-5"
+                  src="@/assets/airplane.jpg"
+                  alt="..."
+                />
+              </div>
+            </div>
+          </div>
+        </header>
+      </div>
+    </div>
+    <!-- 관광지 드롭다운메뉴 끝-->
+  </section>
+</template>
+
+<script>
+export default {
+  name: "AppMain",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped>
+.underline-steelblue {
+  display: inline-block;
+  background: linear-gradient(
+    180deg,
+    rgba(255, 255, 255, 0) 70%,
+    rgba(72, 190, 233, 0.3) 30%
+  );
+}
+</style>
