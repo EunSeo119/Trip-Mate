87720b2e5e8ac58455d714b715077af3544c11b6
EunSeo119 <qnrqjrm03@naver.com>
2023-05-23 17:58:16 +0900

    중간저장

--- EnjoyTrip-Front/src/api/board.js
+++ EnjoyTrip-Front/src/api/board.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+// import axios from "axios";
 import { apiInstance } from "./index.js";
 
 const api = apiInstance();
--- EnjoyTrip-Front/src/router/index.js
+++ EnjoyTrip-Front/src/router/index.js
@@ -43,7 +43,7 @@ const routes = [
   {
     path: "/travel",
     name: "travel",
-    beforeEnter: onlyAuthUser,
+    // beforeEnter: onlyAuthUser,
     component: AppTravel,
   },
   {
